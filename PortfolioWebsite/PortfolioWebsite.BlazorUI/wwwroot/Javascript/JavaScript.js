0b685b2e585b0cad50eb82f86828f8e232107e46
Marcelo <j.marcelo.pinto.98@gmail.com>
2021-01-19 15:37:15 +0000

    Added JSInterop
    Added first version of JSInterop system that enables transition animations.

---
+++ PortfolioWebsite/PortfolioWebsite.BlazorUI/wwwroot/Javascript/JavaScript.js
@@ -0,0 +1,49 @@
+﻿function myFunction(param) {
+    param.forEach(value => {
+        console.log(value);
+    })
+}
+
+
+function MainCaller(elementsToObserve, cssList) {
+    let options = {
+        root: null,
+        rootMargin: '-15px',
+        threshold: 1
+    };
+
+    let observer = new IntersectionObserver(function
+        (entries, self) {
+
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                onEnterViewportCallback(entry, cssList);
+            }
+            else {
+                onExitViewportCallback(entry, cssList);
+            }
+        });
+    }, options);
+
+    elementsToObserve.forEach(element => {
+        observer.observe(document.getElementById(element));
+    });
+}
+
+function onEnterViewportCallback(entry, cssList) {
+    let element = document.getElementById(entry.target.id);
+
+    cssList.forEach(cssClass => {
+        element.classList.add(cssClass);
+        console.log("Entered viewport. Added css: " + cssClass);
+    });
+}
+
+function onExitViewportCallback(entry, cssList) {
+    let element = document.getElementById(entry.target.id);
+
+    cssList.forEach(cssClass => {
+        element.classList.remove(cssClass);
+        console.log("Exited viewport. Removed css: " + cssClass);
+    });
+}
