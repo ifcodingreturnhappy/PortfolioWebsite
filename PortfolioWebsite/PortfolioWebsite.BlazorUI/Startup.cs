5a05d16bdf123a3124eee650f701d81a2991ba77
Marcelo <j.marcelo.pinto.98@gmail.com>
2021-02-13 19:21:42 +0000

    Added necessary changes for Web Hosting
    Added necessary Forward Headers on Startup file.

--- PortfolioWebsite/PortfolioWebsite.BlazorUI/Startup.cs
+++ PortfolioWebsite/PortfolioWebsite.BlazorUI/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -53,6 +54,11 @@ namespace PortfolioWebsite.BlazorUI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
