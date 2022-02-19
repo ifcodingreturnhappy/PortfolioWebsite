79f7d89020f363fa2b0be1137cead7e402b68e31
ifcodingreturnhappy <74977179+ifcodingreturnhappy@users.noreply.github.com>
2022-02-19 16:43:36 +0000

    Deploying to gh-pages from  @ 8556fc50340fed6290becf368f063588d6b02ca7 🚀

---
+++ Javascript/JavaScript.js
@@ -0,0 +1,75 @@
+﻿
+let options = {
+    root: null,
+    rootMargin: '-20px -20px -20px -20px',
+    threshold: 0.3
+};
+
+let currentId = 0;
+
+function setupViewportAnimations(jsonAnimationSettings) {
+    try {
+        let animationSettings = JSON.parse(jsonAnimationSettings);
+
+        let observer = new IntersectionObserver(function
+            (entries, self) {
+
+            entries.forEach(entry => {
+                animationSettings.forEach(animationType => {
+                    if (entry.target.classList.contains(animationType.animationId)) {
+                        observerCallback(entry, animationType.animationCSS);
+                    }
+                });
+            });
+        }, options);
+
+        animationSettings.forEach(animationType => {
+            let elements = document.querySelectorAll('.' + animationType.animationId);
+
+            elements.forEach(element => {
+                if (element.id.length <= 0) {
+                    element.id = ('VPA' + currentId);
+                    currentId++;
+
+                    observer.observe(element);
+                }
+            });
+        });
+    } catch (e) {
+        console.log("Unable to set viewport animation.")
+    }
+}
+
+function observerCallback(entry, cssClass, isRepeatable) {
+    if (entry.isIntersecting) {
+        if (!entry.target.classList.contains(cssClass)) {
+            entry.target.classList.add(cssClass);
+        }
+    }
+
+    //console.log("Observer callback");
+}
+
+
+function updateCurrentPageIndicator() {
+    return;
+
+    let navLinks = document.querySelectorAll('.selectable-item');
+
+    let currentPageRef = window.location.pathname;
+
+    navLinks.forEach(navLink => {
+        let currentNavLink = navLink.getAttribute('href');
+
+        if (currentPageRef.includes(currentNavLink)) {
+            navLink.classList.add('selected-element');
+        }
+        else {
+            navLink.classList.remove('selected-element');
+        }
+    });
+}
+
+function logMessage(message) {
+    console.log(message);
+}
