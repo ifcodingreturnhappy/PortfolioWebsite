407609d46058fe58f11cc9981f991465882db400
Marcelo Pinto <74977179+ifcodingreturnhappy@users.noreply.github.com>
2025-12-30 18:46:40 +0000

    add latest article

--- src/PortfolioWebsite/PortfolioWebsite.BlazorUI/Models/Common/ContentImageInfoModel.cs
+++ src/PortfolioWebsite/PortfolioWebsite.BlazorUI/Models/Common/ContentImageInfoModel.cs
@@ -5,6 +5,7 @@
         public string Url { get; set; }
         public string AltText { get; set; }
         public string OverlayGifUrl { get; set; }
+        public string OverlayGifWidth { get; set; }
 
         public bool HasGifOverlay() => !string.IsNullOrWhiteSpace(OverlayGifUrl);
     }
